dbdb11594c5b792c3bb189a55a26412ce668eca0
Alexander Koskovich <akoskovich@pm.me>
2026-01-17 16:25:05 -0500

    drm/msm/adreno: Fix a6xx_gmu_gx_is_on() for a8xx
    Signed-off-by: Alexander Koskovich <akoskovich@pm.me>

--- drivers/gpu/drm/msm/adreno/a6xx_gmu.c
+++ drivers/gpu/drm/msm/adreno/a6xx_gmu.c
@@ -105,9 +105,12 @@ bool a6xx_gmu_gx_is_on(struct a6xx_gmu *gmu)
 	if (adreno_has_gmu_wrapper(adreno_gpu))
 		return true;
 
-	val = gmu_read(gmu, REG_A6XX_GMU_SPTPRAC_PWR_CLK_STATUS);
+	if (adreno_is_a8xx(adreno_gpu))
+		val = gmu_read(gmu, REG_A8XX_GMU_PWR_CLK_STATUS);
+	else
+		val = gmu_read(gmu, REG_A6XX_GMU_SPTPRAC_PWR_CLK_STATUS);
 
-	if (adreno_is_a7xx(adreno_gpu))
+	if (adreno_is_a7xx(adreno_gpu) || adreno_is_a8xx(adreno_gpu))
 		return !(val &
 			(A7XX_GMU_SPTPRAC_PWR_CLK_STATUS_GX_HM_GDSC_POWER_OFF |
 			A7XX_GMU_SPTPRAC_PWR_CLK_STATUS_GX_HM_CLK_OFF));
